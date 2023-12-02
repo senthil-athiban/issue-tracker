380996d29d1250f86c4c61aed3cfb0455837eb6e
senthil-k8s <senthilathiban0@gmail.com>
2023-12-02 21:21:02 +0530

    Implement Client Side Validation

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,25 +1,31 @@
 "use client";
 import React, {useState} from 'react'
-import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {z} from "zod";
+import { createIssueSchema } from '@/app/validationSchema';
 
+// interface IssueForm {
+//   title: string,
+//   description: string
+// }
 
-interface IssueForm {
-  title: string,
-  description: string
-}
+// validating the type of schema
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
-  const {register, control, handleSubmit} = useForm<IssueForm>();
+  const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
   const [error, setError] = useState("")
   const router = useRouter();  
   
   return (
-
     /* 
     fetching the data from input field and 
     Sending it back to the api using axios
@@ -41,12 +47,13 @@ const NewIssuePage = () => {
         <TextField.Root>
             <TextField.Input placeholder='title' {...register('title')} />
         </TextField.Root>
-
+        {errors.title && <Text color='red' as="p">{errors.title.message}</Text>}
         <Controller 
           name='description'
           control={control}
           render={ ( { field }) => <SimpleMDE placeholder='Description here' {...field} /> }
         />    
+        {errors.description && <Text color='red' as="p">{errors.description.message}</Text>}
         <Button>Submit New Issue</Button>
     </form>
     </div>
